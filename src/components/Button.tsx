b072f6a119ee49206212d01211c049630c419dda
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-12-29 09:57:59 -0300

    style: change text color to equal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,12 +8,10 @@ export interface IButtonProps {
 export function Button({ text, type }: IButtonProps) {
   return (
     <Pressable>
-      {({
-        isPressed
-      }) => {
+      {({ isPressed }) => {
         return (
           <Circle bg={type === 'equal' ? (isPressed ? 'primary.800' : 'primary.900') : (isPressed ? 'background.600' : 'background.800')} size={20} _text={{
-            color: type === 'operator' ? 'primary.800' : type === 'clear' ? 'error.400' : 'text.900',
+            color: type === 'operator' ? 'primary.800' : type === 'clear' ? 'error.400' : type === 'equal' ? 'white' : 'text.900',
             fontSize: 28,
             style: {
               transform: [{
